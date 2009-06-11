0bed6859a27e2b04c9eb42cc391b238e98b362b7
kennytm <kennytm@184b2048-54e1-11de-bf49-a932634fd5a9>
2009-06-11 10:26:42 +0000

    tempobj.hpp modified to support C++03

--- cpp/tempobj.hpp
+++ cpp/tempobj.hpp
@@ -1,6 +1,6 @@
 /*
 
-tempobj.hpp ... Dealing with temporary objects efficiently using move semantic.
+tempobj.hpp ... Dealing with temporary objects efficiently using move semantic. Supports C++03 and C++0x.
 
 Copyright (C) 2009  KennyTM~
 
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
  MEMORY_MANAGER_IMPLEMENTATION(SomeWrapper,						// <-- This will automatically construct the copy/move constructors/assignment operators, and the destructor.
 	 someExpensiveObject.copyFrom(other.someExpensiveObject),	// <-- This is the core statement which an object is deep-copied. 
-	 someExpensiveObject = STD_MOVE(other.someExpensiveObject),	// <-- This is the core statement which an object's ownership is transferred. Always use STD_MOVE to move objects.
+	 someExpensiveObject = std::move(other.someExpensiveObject), // <-- This is the core statement which an object's ownership is transferred. Always use std::move to move objects.
 	 someExpensiveObject.destroy()								// <-- This is the core statement which an object is definitely need to be destroyed.
  );
  
@@ -69,23 +69,49 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #if __GXX_EXPERIMENTAL_CXX0X__ || __cplusplus > 199711L
 #include <memory>
-
-#define STD_MOVE std::move
+#define TMPOBJ(cls) cls&&
+#define MMMOBJ(cls) cls&&
+#define TMPOBJ_INTERFACE
+#define TMPOBJ_IMPLEMENTATION(cls)
+#define CAST_FOR_MMMOVE(cls)
+#define STD_MOVE(cls) std::move
+#define IMMOBJ(cls) cls
+#else
+namespace std {
+	template <typename T>
+	T& move (T& a) { return a; }
+	template <typename T>
+	const T& move (const T& a) { return a; }
+}
+#define TMPOBJ(cls) const cls::Temporary&
+#define MMMOBJ(cls) cls::Temporary&
+#define TMPOBJ_INTERFACE class Temporary
+#define TMPOBJ_IMPLEMENTATION(cls) class cls::Temporary : public cls {          \
+public:                                                                         \
+	Temporary(const cls& other) : cls(other) {}                                 \
+}
+#define CAST_FOR_MMMOVE(cls) const_cast<cls::Temporary&>
+#define STD_MOVE(cls) static_cast<cls::Temporary&>
+#define IMMOBJ(cls) cls::Temporary
+#endif
 
 #define MEMORY_MANAGER_INTERFACE(cls)                                           \
+public:                                                                         \
+	TMPOBJ_INTERFACE;                                                           \
 private:                                                                        \
 	bool mm_dont_dealloc;                                                       \
 	void mm_copy(const cls& other);                                             \
 	void mm_dealloc();                                                          \
-	void mm_move(cls&& other);                                                  \
+	void mm_move(MMMOBJ(cls) other);                                            \
 public:                                                                         \
 	cls(const cls& other);                                                      \
-	cls(cls&& other);                                                           \
+	cls(TMPOBJ(cls) other);                                                     \
 	~cls();                                                                     \
 	cls& operator=(const cls& other);                                           \
-	cls& operator=(cls&& other)
+	cls& operator=(TMPOBJ(cls) other)
 
 #define MEMORY_MANAGER_IMPLEMENTATION_ATTR(attrib, cls, copy_statement, move_statement, destroy_statement) \
+TMPOBJ_IMPLEMENTATION(cls);                                                          \
 attrib void cls::mm_copy(const cls& other) {                                    \
 	mm_dont_dealloc = other.mm_dont_dealloc;                                    \
 	copy_statement;                                                             \
@@ -96,13 +122,13 @@ attrib void cls::mm_dealloc() {                                                 
 		destroy_statement;                                                      \
 	}                                                                           \
 }                                                                               \
-attrib void cls::mm_move(cls&& other) {                                         \
+attrib void cls::mm_move(MMMOBJ(cls) other) {                                   \
 	mm_dont_dealloc = other.mm_dont_dealloc;                                    \
 	move_statement;                                                             \
 	other.mm_dont_dealloc = true;                                               \
 }                                                                               \
 attrib cls::cls(const cls& other) { mm_copy(other); }                           \
-attrib cls::cls(cls&& other) { mm_move(other); }                                \
+attrib cls::cls(TMPOBJ(cls) other) { mm_move(CAST_FOR_MMMOVE(cls)(other)); }    \
 attrib cls::~cls() { mm_dealloc(); }                                            \
 attrib cls& cls::operator=(const cls& other) {                                  \
 	if (this != &other) {                                                       \
@@ -111,10 +137,10 @@ attrib cls& cls::operator=(const cls& other) {                                  
 	}                                                                           \
 	return *this;                                                               \
 }                                                                               \
-attrib cls& cls::operator=(cls&& other) {                                       \
+attrib cls& cls::operator=(TMPOBJ(cls) other) {                                 \
 	if (this != &other) {                                                       \
 		mm_dealloc();                                                           \
-		mm_move(other);                                                         \
+		mm_move(CAST_FOR_MMMOVE(cls)(other));                                   \
 	}                                                                           \
 	return *this;                                                               \
 }
@@ -124,25 +150,18 @@ attrib cls& cls::operator=(cls&& other) {                                       
 
 #define IMMEDIATE_OPERATOR_INTERFACE(cls, op)                                   \
 cls operator op (const cls& self, const cls& other);                            \
-cls&& operator op (cls&& self, const cls& other)
+TMPOBJ(cls) operator op (TMPOBJ(cls) self, const cls& other)
 
 #define IMMEDIATE_OPERATOR_IMPLEMENTATION_ATTR(attrib, cls, op)                 \
-attrib cls operator op (const cls& self, const cls& other) {                    \
-	cls copy = self;                                                            \
-	return std::move(copy op##= other);                                         \
+attrib IMMOBJ(cls) operator op (const cls& self, const cls& other) {            \
+	IMMOBJ(cls) copy = self;                                                    \
+	return STD_MOVE(cls)(copy op##= other);                                     \
 }                                                                               \
-attrib cls&& operator op (cls&& self, const cls& other) {                       \
-	return std::move(self op##= other);                                         \
+attrib TMPOBJ(cls) operator op (TMPOBJ(cls) self, const cls& other) {           \
+	return STD_MOVE(cls)(CAST_FOR_MMMOVE(cls)(self) op##= other);               \
 }
 #define IMMEDIATE_OPERATOR_IMPLEMENTATION(cls, op) IMMEDIATE_OPERATOR_IMPLEMENTATION_ATTR(, cls, op)
 
 #define COMMON_INIT mm_dont_dealloc(false)
-
-
-#else
-
-// TODO
-
-#endif
 
 #endif

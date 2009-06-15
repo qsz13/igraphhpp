964b8465a849a2fe8c5242d3edd5b3b1dc248a4f
kennytm <kennytm@184b2048-54e1-11de-bf49-a932634fd5a9>
2009-06-15 09:26:43 +0000

    Rename PointerVector to ReferenceVector. Use Reference as value_type instead of pointers

--- trunk/igraph/cpp/referencevector.hpp
+++ trunk/igraph/cpp/referencevector.hpp
@@ -1,6 +1,6 @@
 /*
  
- pointervector.hpp ... Wrapper for igraph pointer vectors 
+ referencevector.hpp ... Wrapper for igraph reference vectors 
  
  Copyright (C) 2009  KennyTM~
  
@@ -20,14 +20,14 @@
  */
 
 /**
- \file pointervector.hpp
- \brief Wrapper for igraph pointer vectors
+ \file referencervector.hpp
+ \brief Wrapper for igraph reference vectors
  \author KennyTM~
  \date June 12th, 2009
  */
 
-#ifndef IGRAPH_POINTERVECTOR_HPP
-#define IGRAPH_POINTERVECTOR_HPP
+#ifndef IGRAPH_REFERENCEVECTOR_HPP
+#define IGRAPH_REFERENCEVECTOR_HPP
 
 #include <igraph/igraph.h>
 #include <igraph/cpp/common.hpp>
@@ -36,46 +36,47 @@
 namespace igraph {
 	
 	/**
-	 \class PointerVector
+	 \class ReferenceVector
 	 \brief Wrapper for igraph pointer vectors
 	 
 	 This class is a wrapper of the igraph_vector_ptr_t type, which is an array of void*.
 	 */
 	template<typename T>
-	class PointerVector {
+	class ReferenceVector {
 	private:
 		igraph_vector_ptr_t _;
+		int delete_all;
 		
 	public:
 		typedef T& value_type;
 
-		MEMORY_MANAGER_INTERFACE_WITH_TEMPLATE(PointerVector, <T>);
-		XXINTRNL_WRAPPER_CONSTRUCTOR_INTERFACE(PointerVector, igraph_vector_ptr_t);
+		MEMORY_MANAGER_INTERFACE_WITH_TEMPLATE(ReferenceVector, <T>);
+		XXINTRNL_WRAPPER_CONSTRUCTOR_INTERFACE(ReferenceVector, igraph_vector_ptr_t);
 	
-		/// Create a PointerVector with "count" elements.
-		PointerVector(const long count) MAY_THROW_EXCEPTION;
+		/// Create a ReferenceVector with "count" elements.
+		ReferenceVector(const long count) MAY_THROW_EXCEPTION;
 		
-		/// Copy a C array into a PointerVector.
-		PointerVector(T** const array, const long count) MAY_THROW_EXCEPTION;
+		/// Copy a C array into a ReferenceVector.
+		ReferenceVector(value_type* const array, const long count) MAY_THROW_EXCEPTION;
 		
-		/// var_arg method to construct the PointerVector
-		PointerVector(const long count, ...) MAY_THROW_EXCEPTION;
+		/// var_arg method to construct the ReferenceVector
+		ReferenceVector(const long count, ...) MAY_THROW_EXCEPTION;
 		
-		/// Wrap a C array as a PointerVector.
-		static PointerVector<T> view(T* const* const array, const long count) throw() {
+		/// Wrap a C array as a ReferenceVector.
+		static ReferenceVector<T> view(value_type const* const array, const long count) throw() {
 			igraph_vector_ptr_t _;
 			igraph_vector_ptr_view(&_, array, count);
-			return PointerVector<T>(_, ::tempobj::OwnershipTransferNoOwnership);
+			return ReferenceVector<T>(_, ::tempobj::OwnershipTransferNoOwnership);
 		}
 		
 		void null() throw();
 		
-		T** ptr() throw() { return VECTOR(_); }
-		T* const& operator[](const long index) const throw() { return VECTOR(_)[index]; }
-		T*& operator[](const long index) throw() { return VECTOR(_)[index]; }
+		value_type* ptr() throw() { return VECTOR(_); }
+		value_type const& operator[](const long index) const throw() { return VECTOR(_)[index]; }
+		value_type& operator[](const long index) throw() { return VECTOR(_)[index]; }
 		
-		T* e(const long index) const throw();
-		void set(const long index, T* const value) throw();
+		value_type& e(const long index) const throw();
+		void set(const long index, value_type const value) throw();
 		
 		bool empty() const throw();
 		long size() const throw();
@@ -83,12 +84,12 @@ namespace igraph {
 		void clear() throw();
 		void reserve(const long new_size) MAY_THROW_EXCEPTION;
 		void resize(const long new_size) MAY_THROW_EXCEPTION;
-		void push_back(T* const e) MAY_THROW_EXCEPTION;
-		void insert(const long pos, T* const e) MAY_THROW_EXCEPTION;
+		void push_back(value_type const e) MAY_THROW_EXCEPTION;
+		void insert(const long pos, value_type const e) MAY_THROW_EXCEPTION;
 		void remove(const long pos) throw();
 		
-		void copy_to(T** store) const throw();
-		void sort(int(*compar)(const T*, const T*));
+		void copy_to(value_type* store) const throw();
+		void sort(int(*compar)(const value_type, const value_type));
 		
 		// STL support.
 		typedef value_type* pointer;
@@ -109,23 +110,23 @@ namespace igraph {
 		reference back() throw() { return *(_.end-1); }
 		const_reference back() const throw() { return *(_.end-1); }
 		
-		/// Print content of the PointerVector.
+		/// Print content of the ReferenceVector.
 		void print() const throw() {
 			for (const_iterator cit = begin(); cit != end(); ++ cit)
 				printf("%p, ", *cit);
 			printf("\n");
 		}
 		
-		/// Perform the function on every object of the PointerVector.
-		void perform(void(*fptr)(T*& obj, void* context), void* context = NULL);
+		/// Perform the function on every object of the ReferenceVector.
+		void perform(void(*fptr)(value_type& obj, void* context), void* context = NULL);
 		
-		bool operator==(const PointerVector<T>& other) const throw();
-		bool operator!=(const PointerVector<T>& other) const throw();
+		bool operator==(const ReferenceVector<T>& other) const throw();
+		bool operator!=(const ReferenceVector<T>& other) const throw();
 		
 	};
 	
 	template<typename T>
-	MEMORY_MANAGER_INTERFACE_EX_WITH_TEMPLATE(PointerVector, <T>);
+	MEMORY_MANAGER_INTERFACE_EX_WITH_TEMPLATE(ReferenceVector, <T>);
 }
 
 #endif
